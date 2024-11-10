6493cc479a38af46f89042bf1eef2b9d5b918be8
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2024-11-10 16:59:15 +0400

    repositories have been created

---
+++ EmploymentAgency/EmploymentAgency.Domain/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploymentAgency.Domain.Repositories;
+
+
+internal interface IRepository<T>
+{
+    public IEnumerable<T> Gets();
+    public T? GetById(int id);
+    public void Post(T entity);
+    public bool Put(int id,T entity);
+    public bool Delete(int id);
+}
---
+++ EmploymentAgency/EmploymentAgency.Domain/Repositories/RepositoryVacancy.cs
@@ -0,0 +1,38 @@
+﻿using EmploymentAgency.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploymentAgency.Domain.Repositories;
+
+public class RepositoryVacancy : IRepository<Vacancy>
+{
+    private readonly List<Vacancy> _vacancies = [];
+
+    public bool Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Vacancy> Gets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Vacancy? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Post(Vacancy entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Put(int id, Vacancy entity)
+    {
+        throw new NotImplementedException();
+    }
+}
