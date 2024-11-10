df651294066b4abd1ff6f6a9f9fddaf163d4a01f
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2024-11-10 20:20:16 +0400

    server project

---
+++ EmploymentAgency/EmploymentAgency.Server/Program.cs
@@ -0,0 +1,30 @@
+
+using EmploymentAgency.Server.DTO;
+using EmploymentAgency.Domain.Models;
+using EmploymentAgency.Domain.Repositories;
+var builder = WebApplication.CreateBuilder(args);
+ 
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddSingleton<RepositoryApplicant>();
+builder.Services.AddAutoMapper(typeof(Mapper));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
