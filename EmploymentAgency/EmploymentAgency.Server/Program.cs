f442fd4574ca49498e491bf94cb70ef503705d28
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2024-11-11 00:22:36 +0400

    Добавлены сервисы IRepository

--- EmploymentAgency/EmploymentAgency.Server/Program.cs
+++ EmploymentAgency/EmploymentAgency.Server/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IRepository<Applicant>, RepositoryApplicant>();
+builder.Services.AddSingleton<IRepository<Employer>, RepositoryEmployer>();
+builder.Services.AddSingleton<IRepository<JobPosition>, RepositoryJobPosition>();
+builder.Services.AddSingleton<IRepository<Response>, RepositoryResponse>();
+builder.Services.AddSingleton<IRepository<Resume>, RepositoryResume>();
+builder.Services.AddSingleton<IRepository<Vacancy>, RepositoryVacancy>();
 builder.Services.AddAutoMapper(typeof(Mapper));
 var app = builder.Build();
 
