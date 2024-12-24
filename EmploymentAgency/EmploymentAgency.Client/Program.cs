cbc5d601b9c0772144408a795b51d466602347a1
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2024-12-25 01:28:29 +0400

    CRUD с Applicant реализован

--- EmploymentAgency/EmploymentAgency.Client/Program.cs
+++ EmploymentAgency/EmploymentAgency.Client/Program.cs
@@ -1,5 +1,6 @@
 using EmploymentAgency.Client;
 using EmploymentAgency.Client.Api;
+using EmploymentAgency.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -14,7 +15,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<UserSessionService>();
 
 builder.Services.AddSingleton<IEmploymentAgencyWrapper, EmploymentAgencyApiWrapper>(); //
 
---
+++ EmploymentAgency/EmploymentAgency.Client/Services/UserSessionService.cs
@@ -0,0 +1,7 @@
+﻿namespace EmploymentAgency.Client.Services;
+
+public class UserSessionService
+{
+    public int? UserId { get; set; }
+    public bool IsApplicant { get; set; }
+}
